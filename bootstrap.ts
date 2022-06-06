316b0cdf7677123b20d22e384e6f5da8e96360f3
Vladislav Knyshov <inova136@gmail.com>
2022-06-07 01:07:05 +0300

    rename import database in bootstrap

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,8 +1,8 @@
-import postgresql from "./database";
 import app from "./app";
+import database from "./database";
 
 const bootstrap = async () => Promise.all([
-  postgresql.initialize().then(() => console.log("Database has been connected.")),
+  database.initialize().then(() => console.log("Database has been connected.")),
   app.initialize().then((port) => console.log(`Server has started on port :${port}`)),
 ]).catch(err => {
   console.log(err.name + ": " + err.message)
