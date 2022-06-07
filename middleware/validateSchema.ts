324939b91a77355b13f44264988888e13b01573a
Vladislav Knyshov <inova136@gmail.com>
2022-06-07 23:25:05 +0300

    remove unused logs in validateScheme middleware

--- middleware/validateSchema.ts
+++ middleware/validateSchema.ts
@@ -4,13 +4,11 @@ import createHTTPError from "http-errors";
 
 const validateSchema =  (schema: AnyObjectSchema): RequestHandler => async (req, res, next) => {
   try {
-    const res = await schema.validate({
+    await schema.validate({
       body: req.body,
       query: req.query,
       params: req.params
     });
-    console.log("Validation:");
-    console.log(res);
     return next();
   } catch (e) {
     return next(createHTTPError(400, (e as ValidationError).message));
