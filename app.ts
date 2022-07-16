a6b34bd3b75422e5e82a438dc367f3085368e2e4
Vladislav Knyshov <inova136@gmail.com>
2022-07-16 11:28:18 +0300

    add cors package & setup cors

--- app.ts
+++ app.ts
@@ -1,40 +1,45 @@
 import createHTTPError from "http-errors";
-import express, {ErrorRequestHandler} from "express";
+import express, { ErrorRequestHandler } from "express";
 import path from "path";
 import logger from "morgan";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import config from "./config";
 
 import authRouter from "./routes/auth";
 import usersRouter from "./routes/users";
 import articlesRouter from "./routes/article";
+import cors from "cors";
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/auth', authRouter);
-app.use('/users', usersRouter);
-app.use('/articles', articlesRouter);
-
-app.use((req, res, next) =>
-  next(createHTTPError(404))
+app.use(
+  cors({
+    origin: config.ORIGIN,
+  })
 );
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/auth", authRouter);
+app.use("/users", usersRouter);
+app.use("/articles", articlesRouter);
+
+app.use((req, res, next) => next(createHTTPError(404)));
 
 app.use(((err, req, res, next) => {
   const status = err.status || 500;
   res.status(status).json({
     status,
-    message: err.message || "Unknown error"
-  })
-}) as ErrorRequestHandler)
+    message: err.message || "Unknown error",
+  });
+}) as ErrorRequestHandler);
 
 export default {
-  initialize: () => new Promise((resolve) =>
-    app.listen(config.PORT,() => resolve(config.PORT))
-  )
-}
+  initialize: () =>
+    new Promise((resolve) =>
+      app.listen(config.PORT, () => resolve(config.PORT))
+    ),
+};
--- config.ts
+++ config.ts
@@ -1,11 +1,12 @@
 export default {
+  ORIGIN: process.env.ORIGIN || "http://localhost:3000",
   PORT: process.env.PORT || 3000,
   DB_PORT: Number(process.env.DB_PORT) || 5432,
   DB_USERNAME: process.env.DB_USERNAME || "root",
   DB_PASSWORD: process.env.DB_PASSWORD || "root",
   DB_NAME: process.env.DB_NAME || "root",
-  DB_HOST : process.env.DB_HOST || "localhost",
-  DB_TYPE:  process.env.DB_TYPE || "postgres",
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_TYPE: process.env.DB_TYPE || "postgres",
   PASSWORD_SALT: process.env.PASSWORD_SALT || "salt",
   IS_PRODUCTION: process.env.NODE_ENV == "production",
-}
+};
