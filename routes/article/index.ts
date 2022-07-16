01b29606b7a9cac9328a649ebcd3f63d81338428
Vladislav Knyshov <inova136@gmail.com>
2022-07-16 11:28:43 +0300

    fix articles /get route

--- routes/article/index.ts
+++ routes/article/index.ts
@@ -1,156 +1,226 @@
-import {Router} from "express";
-import {authGuard, RequestWithAuthData} from "../../middleware/authGuard";
+import { Router } from "express";
+import { authGuard, RequestWithAuthData } from "../../middleware/authGuard";
 import validateSchema from "../../middleware/validateSchema";
 import {
   createArticleRequestScheme,
   deleteArticleRequestScheme,
   getArticlesRequestQuery,
   getArticlesRequestScheme,
-  patchArticleRequestScheme
+  patchArticleRequestScheme,
 } from "./schemes";
 import filterObject from "../../utils/filterObject";
-import {articleRepository} from "../../database";
-import createHTTPError, {HttpError} from "http-errors";
-import {userRole} from "../../entities/user";
-import Article, {articleState} from "../../entities/article";
-import {DeepPartial} from "typeorm";
+import { articleRepository } from "../../database";
+import createHTTPError, { HttpError } from "http-errors";
+import { userRole } from "../../entities/user";
+import Article, { articleState } from "../../entities/article";
+import { DeepPartial } from "typeorm";
 import canUserModifyTheArticle from "./canUserModifyTheArticle";
 
 const router = Router();
 
 // Get articles
-router.get('', authGuard, validateSchema(getArticlesRequestScheme), async (req, res, next) => {
-  const {title, authorId, text, state, skip, limit} = req.query;
-  const searchOptions = filterObject<getArticlesRequestQuery>({title, authorId, text, state, skip, limit});
+router.get(
+  "",
+  validateSchema(getArticlesRequestScheme),
+  async (req, res, next) => {
+    const { title, authorId, text, state, skip, limit } = req.query;
+    const searchOptions = filterObject<getArticlesRequestQuery>({
+      title,
+      authorId,
+      text,
+      state,
+      skip,
+      limit,
+    });
 
-  try {
-    const articlesQueryBuilder = articleRepository.createQueryBuilder("a")
-    .leftJoinAndSelect("a.author", "author")
-    .select(["a.title", "a.thumbnailText", "a.text", "a.id", "author.id", "author.name"])
-    .offset(Number(searchOptions.skip))
-    .limit(Number(searchOptions.limit))
+    try {
+      const articlesQueryBuilder = articleRepository
+        .createQueryBuilder("a")
+        .leftJoinAndSelect("a.author", "author")
+        .select([
+          "a.title",
+          "a.thumbnailText",
+          "a.text",
+          "a.id",
+          "author.id",
+          "author.name",
+        ])
+        .offset(Number(searchOptions.skip))
+        .limit(Number(searchOptions.limit));
 
-    if(searchOptions.state) {
-      articlesQueryBuilder.andWhere({state: searchOptions.state})
+      if (searchOptions.state) {
+        articlesQueryBuilder.andWhere({ state: searchOptions.state });
+      }
+
+      if (searchOptions.authorId) {
+        articlesQueryBuilder.andWhere({
+          author: { id: Number(searchOptions.authorId) },
+        });
+      }
+
+      if (searchOptions.title) {
+        articlesQueryBuilder.andWhere("a.title like :title", {
+          title: `%${searchOptions.title}%`,
+        });
+      }
+
+      if (searchOptions.text) {
+        articlesQueryBuilder.andWhere("a.text like :text", {
+          text: `%${searchOptions.text}%`,
+        });
+      }
+
+      const articles = await articlesQueryBuilder.getMany();
+      res.status(200).json({
+        ok: true,
+        articles,
+      });
+    } catch (e) {
+      console.log(e);
+      next(
+        createHTTPError(500, "Error while getting articles from the database.")
+      );
     }
-
-    if (searchOptions.authorId) {
-      articlesQueryBuilder.andWhere({author: {id: Number(searchOptions.authorId)}});
-    }
-
-    if (searchOptions.title) {
-      articlesQueryBuilder.andWhere("a.title like :title", {title: `%${searchOptions.title}%`});
-    }
-
-    if (searchOptions.text) {
-      articlesQueryBuilder.andWhere("a.text like :text", {text: `%${searchOptions.text}%`});
-    }
-
-    const articles = await articlesQueryBuilder.getMany();
-    res.status(200).json({
-      ok: true,
-      articles
-    })
-  } catch (e) {
-    console.log(e);
-    next(createHTTPError(500, "Error while getting articles from the database."));
   }
-});
+);
 
 // Create new article
-router.post('', authGuard, validateSchema(createArticleRequestScheme), async (req, res, next) => {
-  const {title, text, thumbnailText} = req.body;
-  const user = (req as RequestWithAuthData).user;
-  const newArticleFields: DeepPartial<Article> = {
-    title, text, thumbnailText,
-    author: user
-  };
+router.post(
+  "",
+  authGuard,
+  validateSchema(createArticleRequestScheme),
+  async (req, res, next) => {
+    const { title, text, thumbnailText } = req.body;
+    const user = (req as RequestWithAuthData).user;
+    const newArticleFields: DeepPartial<Article> = {
+      title,
+      text,
+      thumbnailText,
+      author: user,
+    };
 
-  // Admin can create articles with custom state
-  if (user.role == userRole.ADMIN) {
-    newArticleFields.state = req.body.state;
-  }
-
-  try {
-    const newArticle = articleRepository.create(newArticleFields);
-    const article = await articleRepository.save(newArticle);
-    return res.status(200).json({
-      ok: true,
-      article: {
-        ...article,
-        author: user.data
-      }
-    });
-  } catch (e) {
-    // @ts-ignore
-    if (e.code === "23505") {
-      return next(createHTTPError(400, "An article with the same title already exists."));
+    // Admin can create articles with custom state
+    if (user.role == userRole.ADMIN) {
+      newArticleFields.state = req.body.state;
     }
-    return next(createHTTPError(500, "Error while saving a new article to the database."));
+
+    try {
+      const newArticle = articleRepository.create(newArticleFields);
+      const article = await articleRepository.save(newArticle);
+      return res.status(200).json({
+        ok: true,
+        article: {
+          ...article,
+          author: user.data,
+        },
+      });
+    } catch (e) {
+      // @ts-ignore
+      if (e.code === "23505") {
+        return next(
+          createHTTPError(400, "An article with the same title already exists.")
+        );
+      }
+      return next(
+        createHTTPError(
+          500,
+          "Error while saving a new article to the database."
+        )
+      );
+    }
   }
-});
+);
 
 // Update an article
-router.patch('', authGuard, validateSchema(patchArticleRequestScheme), async (req, res, next) => {
-  const {title, text, thumbnailText, state, id} = req.body;
-  const user = (req as RequestWithAuthData).user;
-  const userIsModeratorOrAdmin = [userRole.ADMIN, userRole.MODERATOR].includes(user.role);
+router.patch(
+  "",
+  authGuard,
+  validateSchema(patchArticleRequestScheme),
+  async (req, res, next) => {
+    const { title, text, thumbnailText, state, id } = req.body;
+    const user = (req as RequestWithAuthData).user;
+    const userIsModeratorOrAdmin = [
+      userRole.ADMIN,
+      userRole.MODERATOR,
+    ].includes(user.role);
 
-  // Check if user can update the article
-  const mayBeError = await canUserModifyTheArticle(user, id);
-  if(mayBeError instanceof HttpError) {
-    return next(mayBeError);
-  }
+    // Check if user can update the article
+    const mayBeError = await canUserModifyTheArticle(user, id);
+    if (mayBeError instanceof HttpError) {
+      return next(mayBeError);
+    }
 
-  const fieldsToUpdate = filterObject<DeepPartial<Article>>({
-    title, text, thumbnailText, state
-  });
-
-  // Only Admin or moderator can publish articles
-  if(!userIsModeratorOrAdmin && fieldsToUpdate.state == articleState.PUBLISHED) {
-    delete fieldsToUpdate.state;
-  }
-
-  try {
-    const articleWithUpdatedFields = articleRepository.create(fieldsToUpdate);
-    const updateArticleResult = await articleRepository.createQueryBuilder("article")
-    .update(articleWithUpdatedFields)
-    .where("id = :id", {id})
-    .returning('*')
-    .updateEntity(true)
-    .execute();
-
-    if (!updateArticleResult.raw[0]) throw new Error();
-
-    const article = articleRepository.create(updateArticleResult.raw[0] as DeepPartial<Article>);
-    return res.status(200).json({
-      ok: true,
-      article,
+    const fieldsToUpdate = filterObject<DeepPartial<Article>>({
+      title,
+      text,
+      thumbnailText,
+      state,
     });
-  } catch (e) {
-    return next(createHTTPError(500, "Error while saving the article to the database."));
+
+    // Only Admin or moderator can publish articles
+    if (
+      !userIsModeratorOrAdmin &&
+      fieldsToUpdate.state == articleState.PUBLISHED
+    ) {
+      delete fieldsToUpdate.state;
+    }
+
+    try {
+      const articleWithUpdatedFields = articleRepository.create(fieldsToUpdate);
+      const updateArticleResult = await articleRepository
+        .createQueryBuilder("article")
+        .update(articleWithUpdatedFields)
+        .where("id = :id", { id })
+        .returning("*")
+        .updateEntity(true)
+        .execute();
+
+      if (!updateArticleResult.raw[0]) throw new Error();
+
+      const article = articleRepository.create(
+        updateArticleResult.raw[0] as DeepPartial<Article>
+      );
+      return res.status(200).json({
+        ok: true,
+        article,
+      });
+    } catch (e) {
+      return next(
+        createHTTPError(500, "Error while saving the article to the database.")
+      );
+    }
   }
-});
+);
 
 // Delete an article
-router.delete('', authGuard, validateSchema(deleteArticleRequestScheme), async (req, res, next) => {
-  const {id} = req.body;
-  const user = (req as RequestWithAuthData).user;
+router.delete(
+  "",
+  authGuard,
+  validateSchema(deleteArticleRequestScheme),
+  async (req, res, next) => {
+    const { id } = req.body;
+    const user = (req as RequestWithAuthData).user;
 
-  // Check if user can update the article
-  const mayBeError = await canUserModifyTheArticle(user, id);
-  if(mayBeError instanceof HttpError) {
-    return next(mayBeError);
-  }
+    // Check if user can update the article
+    const mayBeError = await canUserModifyTheArticle(user, id);
+    if (mayBeError instanceof HttpError) {
+      return next(mayBeError);
+    }
 
-  try {
-    await articleRepository.delete(id);
-    return res.status(200).json({
-      ok: true
-    });
-  } catch(e) {
-    return next(createHTTPError(500, "Error while deleting the article from the database."));
+    try {
+      await articleRepository.delete(id);
+      return res.status(200).json({
+        ok: true,
+      });
+    } catch (e) {
+      return next(
+        createHTTPError(
+          500,
+          "Error while deleting the article from the database."
+        )
+      );
+    }
   }
-});
+);
 
 export default router;
--- routes/article/schemes.ts
+++ routes/article/schemes.ts
@@ -1,5 +1,5 @@
-import * as yup from "yup"
-import {articleState} from "../../entities/article";
+import * as yup from "yup";
+import { articleState } from "../../entities/article";
 
 export const getArticlesRequestScheme = yup.object({
   query: yup.object({
@@ -9,25 +9,28 @@ export const getArticlesRequestScheme = yup.object({
     state: yup.string().oneOf(Object.keys(articleState)),
     skip: yup.string().default("0"),
     limit: yup.string().default("100"),
-  })
-})
+  }),
+});
 
 export type getArticlesRequestQuery = {
-  title?: string,
-  authorId?: string,
-  text?: string,
-  state: articleState,
-  skip: string,
-  limit: string,
-}
+  title?: string;
+  authorId?: string;
+  text?: string;
+  state?: articleState;
+  skip: string;
+  limit: string;
+};
 
 export const createArticleRequestScheme = yup.object({
   body: yup.object({
     title: yup.string().required(),
     thumbnailText: yup.string(),
     text: yup.string().required(),
-    state: yup.string().oneOf(Object.keys(articleState)).default(articleState.DRAFT),
-  })
+    state: yup
+      .string()
+      .oneOf(Object.keys(articleState))
+      .default(articleState.DRAFT),
+  }),
 });
 
 export const patchArticleRequestScheme = yup.object({
@@ -37,11 +40,11 @@ export const patchArticleRequestScheme = yup.object({
     thumbnailText: yup.string(),
     text: yup.string(),
     state: yup.string().oneOf(Object.keys(articleState)),
-  })
+  }),
 });
 
 export const deleteArticleRequestScheme = yup.object({
   body: yup.object({
     id: yup.number().required(),
-  })
+  }),
 });
