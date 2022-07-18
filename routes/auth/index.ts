577fead7f360847bf43bb70215ce6a64663de765
Vladislav Knyshov <inova136@gmail.com>
2022-07-18 14:46:24 +0300

    fix cookie setting

--- routes/auth/index.ts
+++ routes/auth/index.ts
@@ -1,93 +1,118 @@
-import {Router} from "express";
-import {authGuard, noAuthGuard, RequestWithAuthData} from "../../middleware/authGuard";
-import {sessionRepository, userRepository} from "../../database";
+import { Router } from "express";
+import {
+  authGuard,
+  noAuthGuard,
+  RequestWithAuthData,
+} from "../../middleware/authGuard";
+import { sessionRepository, userRepository } from "../../database";
 import createHTTPError from "http-errors";
 import validateSchema from "../../middleware/validateSchema";
-import {loginRequestSchema, registerRequestSchema} from "./schemes";
+import { loginRequestSchema, registerRequestSchema } from "./schemes";
 import User from "../../entities/user";
 import generateSalt from "../../utils/generateSalt";
 
+const WEEK = 6.048e8;
 const router = Router();
 
-router.post('/register', noAuthGuard, validateSchema(registerRequestSchema), async (req, res, next) => {
-  const {name, email, password} = req.body;
+router.post(
+  "/register",
+  noAuthGuard,
+  validateSchema(registerRequestSchema),
+  async (req, res, next) => {
+    const { name, email, password } = req.body;
 
-  try {
-    const salt = generateSalt();
-    const user = userRepository.create({
-      name,
-      email,
-      salt,
-      passwordHash: await User.hashPassword(password, salt)
-    });
-    await userRepository.save(user);
-  } catch (e) {
-    // @ts-ignore
-    if (e.code === "23505") {
-      return next(createHTTPError(400, "User with the same email already exists."));
-    }
-    return next(createHTTPError(500, "Error while saving user on server."))
-  }
-
-  return res.status(200).send({
-    ok: true,
-  });
-});
-
-router.post('/login', noAuthGuard, validateSchema(loginRequestSchema), async (req, res, next) => {
-  const {email, password} = req.body;
-
-  const user = await userRepository.findOneBy({email});
-  if (!user) {
-    return next(createHTTPError(400, "Email or password is incorrect."));
-  }
-
-  const isPasswordValid = await user.validatePassword(password);
-  if (!isPasswordValid) {
-    return next(createHTTPError(400, "Email or password is incorrect."));
-  }
-
-  try {
-    let session = await sessionRepository.findOne({
-      relations: ["user"],
-      where: {
-        user: {
-          id: user.id
-        }
+    try {
+      const salt = generateSalt();
+      const user = userRepository.create({
+        name,
+        email,
+        salt,
+        passwordHash: await User.hashPassword(password, salt),
+      });
+      await userRepository.save(user);
+    } catch (e) {
+      // @ts-ignore
+      if (e.code === "23505") {
+        return next(
+          createHTTPError(400, "User with the same email already exists.")
+        );
       }
-    });
-    if(!session) {
-      session = sessionRepository.create({user});
-      await sessionRepository.save(session);
+      return next(createHTTPError(500, "Error while saving user on server."));
     }
-    res.cookie("session", session.id, {httpOnly: true});
-  } catch (e) {
-    return next(createHTTPError(500, "Error while saving session on server."))
+
+    return res.status(200).send({
+      ok: true,
+    });
   }
+);
 
-  return res.status(200).json({
-    ok: true, user: user.data
-  });
-});
+router.post(
+  "/login",
+  noAuthGuard,
+  validateSchema(loginRequestSchema),
+  async (req, res, next) => {
+    const { email, password } = req.body;
 
-router.post('/logout', authGuard, async (req, res, next) => {
+    const user: User | null = await userRepository.findOneBy({ email });
+    if (!user) {
+      return next(createHTTPError(400, "Email or password is incorrect."));
+    }
+
+    const isPasswordValid = await user.validatePassword(password);
+    if (!isPasswordValid) {
+      return next(createHTTPError(400, "Email or password is incorrect."));
+    }
+
+    try {
+      let session = await sessionRepository.findOne({
+        relations: ["user"],
+        where: {
+          user: {
+            id: user.id,
+          },
+        },
+      });
+      if (!session) {
+        session = sessionRepository.create({ user });
+        await sessionRepository.save(session);
+      }
+
+      res.cookie("session", session.id, {
+        httpOnly: true,
+        maxAge: WEEK,
+        path: "/",
+      });
+    } catch (e) {
+      return next(
+        createHTTPError(500, "Error while saving session on server.")
+      );
+    }
+
+    return res.status(200).json({
+      ok: true,
+      user: user.data,
+    });
+  }
+);
+
+router.post("/logout", authGuard, async (req, res, next) => {
   res.clearCookie("session");
   const session = (req as RequestWithAuthData).session;
   try {
-    await sessionRepository.delete({ id: session.id })
+    await sessionRepository.delete({ id: session.id });
   } catch (e) {
     next(createHTTPError(500, (e as Error).message));
   }
 
   return res.status(200).json({
-    ok: true
+    ok: true,
   });
 });
 
-router.get('/me', authGuard, async (req, res) => {
+router.get("/me", authGuard, async (req, res) => {
   return res.status(200).json({
     ok: true,
-    user: (req as RequestWithAuthData).user.data
+    user: (req as RequestWithAuthData).user.data,
   });
 });
 
